32063d328f74c84d299375a3a61503f98d96ef3b
beyaz1981 <47332884+beyaz1981@users.noreply.github.com>
2021-04-01 23:53:18 +0300

    Create WebSettings.cs

---
+++ WebSettings.cs
@@ -0,0 +1,23 @@
+public static class TroyaRestDemo
+    {
+        public static string BaseURL = "https://uat-sgw.thy.com:8443/mws-rest";
+        public static string RestBaseURL = "https://uat-sgw.thy.com:8443/websagent-rest";
+        public static string ClientUsername = "WEBSDOM";
+        public static string apikey = "";
+        public static string apisecret = "";
+        public static string UserName = "username";
+        public static string Password = "password";
+        public static string BasicAuth = "Basic username:password";
+        public static string Channel = "WEB";
+        public static string TargetSource = "BrandedFares";
+        public static string WindowAfter = "P0D";
+        public static string WindowBefore = "P0D";
+        public static string AgencyOfficeCode = "ITT";
+        public static string Application = "IBE";
+        public static string ClientCode = "WEBSDOM";
+        public static bool LogAvailibilityResponse = true;
+        public static bool StoreAvailibilityData = true;
+        public static bool StoreRequestResponse = true;
+        public static bool StoreGetFareInitiate = true;
+        public static int CommisionRate = 20;
+    }
